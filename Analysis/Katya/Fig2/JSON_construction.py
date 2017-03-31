ed2a01438c870f726d8e70ef66233bbe62463dec
katyaputintseva <katya.putintseva@gmail.com>
2017-03-31 13:23:30 +0200

    Fig.2

--- Analysis/Katya/Fig2/JSON_construction.py
+++ Analysis/Katya/Fig2/JSON_construction.py
@@ -8,55 +8,76 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 import distance
+from multiprocessing import Pool
 import json
-pd.set_option('display.max_columns', 100)
 
 
-# In[2]:
+# In[68]:
 
+chunks = ['S'+str(i) for i in range(1,13)]
 data = {}
 clean_data = {}
 
-for f in os.listdir('/home/katya/local/HIS3InterspeciesEpistasis/Data/'):
+for f in os.listdir('/users/fk/eputintseva/projects/HIS/Data/'):
     if 'csv' in f:
-        data[f[:-16]] = pd.DataFrame.from_csv('/home/katya/local/HIS3InterspeciesEpistasis/Data/' + f, sep = '\t')
-        clean_data[f[:-16]] = data[f[:-16]][(data[f[:-16]].nonsense == 0) & (data[f[:-16]].middle == 1)]
+        data[f[:-16]] = pd.DataFrame.from_csv('/users/fk/eputintseva/projects/HIS/Data/' + f, sep = '\t')
+        clean_data[f[:-16]] = data[f[:-16]][(data[f[:-16]].nonsense == 0) & (data[f[:-16]].middle == 1)]        
 
 
-# In[ ]:
+# In[69]:
 
-positions = pd.DataFrame.from_csv('/home/katya/local/HIS3InterspeciesEpistasis/Data_Small_Tables/positions.csv', sep = '\t')
-
-for chunk in clean_data:
-    print (chunk)
+def write_json(chunk):
+    print chunk
+    
+    fitness_threshold_1 = 0.8*clean_data[chunk].s.max()
+    fitness_threshold_2 = 0.6*clean_data[chunk].s.max()
+    fitness_threshold_3 = 0.4*clean_data[chunk].s.max()
+    fitness_threshold_4 = 0.2*clean_data[chunk].s.max()
+    
     sqs = list(clean_data[chunk].index)
     
-    for ind in range(len(sqs)):
-        sqs[ind] = sqs[ind][:int(positions[chunk].ix['len1'])] + sqs[ind][-int(positions[chunk].ix['len2']):]
-        
     data = {}
     data['nodes'] = []
+    counter = 0
     for i in range(len(sqs)):
-        data['nodes'].append({'name':sqs[i], 
-                              'x':(clean_data[chunk].dist_Scer[i] + np.random.normal(0, .1)), 
-                              'y':clean_data[chunk].s[i]*10})
+        counter+=1
+        data['nodes'].append({'name':'%s, distance: %d, fitness: %.2f' % (clean_data[chunk].mut_list[i], clean_data[chunk].dist_Scer[i], clean_data[chunk].s[i]),
+                              'x':(clean_data[chunk].dist_Scer[i] + np.random.normal(0, .05)), 
+                              'y':-(clean_data[chunk].s[i]*10)})
         
     data['connections'] = []
     
     done = []
     for i1 in range(len(sqs)):
         if i1%1000 == 0:
-            print (i1)
+            print i1
         done.append(sqs[i1])
         for i2 in range(len(sqs)):
             if sqs[i2] not in done and clean_data[chunk].dist_Scer[i1]!=clean_data[chunk].dist_Scer[i2]             and distance.hamming(sqs[i1],sqs[i2]) == 1:
-                if clean_data[chunk].s[i1]>=3 and clean_data[chunk].s[i2]>=3:
-                    data['connections'].append({'source':sqs[i1], 'target':sqs[i2], 'color':1})
-                else:
-                    data['connections'].append({'source':sqs[i1], 'target':sqs[i2], 'color':2})
                 
-    with open('/home/katya/local/HIS3InterspeciesEpistasis/Analysis/Katya/Fig2/' + chunk + '.json', 'w+') as outfile:  
+                if clean_data[chunk].s[i1]>=fitness_threshold_1 and clean_data[chunk].s[i2]>=fitness_threshold_1:
+                    data['connections'].append({'source':data['nodes'][i1]['name'], 'target':data['nodes'][i2]['name'], 'color':'#00AEE9'})
+                
+                elif clean_data[chunk].s[i1]>=fitness_threshold_2 and clean_data[chunk].s[i2]>=fitness_threshold_2:
+                    data['connections'].append({'source':data['nodes'][i1]['name'], 'target':data['nodes'][i2]['name'], 'color':'#34ADD3'})
+                
+                elif clean_data[chunk].s[i1]>=fitness_threshold_3 and clean_data[chunk].s[i2]>=fitness_threshold_3:
+                    data['connections'].append({'source':data['nodes'][i1]['name'], 'target':data['nodes'][i2]['name'], 'color':'#70ADC7'})
+                
+                elif clean_data[chunk].s[i1]>=fitness_threshold_4 and clean_data[chunk].s[i2]>=fitness_threshold_4:
+                    data['connections'].append({'source':data['nodes'][i1]['name'], 'target':data['nodes'][i2]['name'], 'color':'#9AABB4'})
+                
+                else:
+                    data['connections'].append({'source':data['nodes'][i1]['name'], 'target':data['nodes'][i2]['name'], 'color':'#A7A9AC'})
+                
+    with open('/users/fk/eputintseva/projects/HIS/Fig2/Build/data/' + chunk + '.json', 'w+') as outfile:  
         json.dump(data, outfile)
+
+
+# In[1]:
+
+pool = Pool()
+pool.map(write_json, chunks)
 
 
 # In[ ]:
