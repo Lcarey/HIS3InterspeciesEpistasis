8035915eb65d2c6134faa74eaa0f83addfa3bab9
Lucas Carey <lucas.carey@upf.edu>
2017-04-24 10:23:39 +0800

    a

--- Analysis/Lucas/EpistasisLBC_Figure_HowManyDimensionsToDisplayMutationalLandscape_PCA.m
+++ Analysis/Lucas/EpistasisLBC_Figure_HowManyDimensionsToDisplayMutationalLandscape_PCA.m
@@ -1,78 +1,85 @@
+%  EpistasisLBC_Figure_HowManyDimensionsToDisplayMutationalLandscape_PCA.m 
 %% how many dimensions are necessary to display mutational landscape of each segment? 
 % calculated using linear PCA (no free params, and exact)
 % hamming distance between mutations
-
-
-s = struct();
+%
+% LBC March 2017
+s=struct();
 for I = 1:12
-    tic;key
-    fprintf('%d... ',I);
-    s(I).T  = EpistasisLBC__LoadData_FindVariableRegion_GenSparseMatrix( I   );
-    s(I).T = s(I).T( : , {'s' 'aa_seq_variable' 'SparseVect'});
-    s(I).lseq = length(s(I).T.aa_seq_variable{1}) ;
+    tic;
+    % natlib
+    T  = EpistasisLBC__LoadData( I  ,'ONLY_NATLIB_FLAG',true, 'ONLY_MIDDLE_FLAG',true,'NO_STOP_FLAG',true);
+    
+    %everything. basically the same result
+    % T  = EpistasisLBC__LoadData( I  ,'ONLY_NATLIB_FLAG',false, 'ONLY_MIDDLE_FLAG',false,'NO_STOP_FLAG',true);
+
+    T  = EpistasisLBC__FindVariableRegion_GenSparseMatrix( T ) ; 
+    s(I).T = T;
     s(I).Segment = I ; 
-    [~,~,~,~, s(I).explained,~] = pca(cell2mat(s(I).T.SparseVect) ,'NumComponents',50,'Centered',true);
-    fprintf('%0.02f min\n' , toc / 60 );
-end
-save('s_for_pca.mat','s');
-
-
-%% calc shannon entropy of each lib ( is 0, useless)
-% calc pca on only non-zero rows
-%    gives the same results, but faster?
-for I = 1:12
-    vmat = cell2mat(s(I).T.SparseVect);
-    s(I).vmat = vmat( : , (mean( vmat) ~=0)  );
-    [~,~,~,~, s(I).explained2,~] = pca(vmat ,'NumComponents',50,'Centered',true);
+    [~,~,~,~, s(I).explained,~] = pca( double(cell2mat(s(I).T.SparseVect)),'Centered',false);
+    fprintf('%d took %0.01f min\n' , I , toc/60); tic;
 end
 
+
+ 
 %%
-for I = 1:12
-    s(I).thresh95_2 = find(cumsum(s(I).explained2)>95,1,'first') ;
-    s(I).thresh95 = find(cumsum(s(I).explained)>95,1,'first') ; 
-    s(I).thresh90_2 = find(cumsum(s(I).explained2)>90,1,'first') ;
-    s(I).thresh90 = find(cumsum(s(I).explained)>90,1,'first') ; 
-    s(I).thresh99_2 = find(cumsum(s(I).explained2)>99,1,'first') ;
+% s = sortStruct(s,'Segment');
+
+
+fewest_dim = min(cellfun(@length,{s.explained})) ; 
+rmat = NaN(  fewest_dim+1, numel(s));
+
+for I = 1:numel(s)
     s(I).thresh99 = find(cumsum(s(I).explained)>99,1,'first') ; 
-    s(I).thresh100_2 = find(cumsum(s(I).explained2)>=100,1,'first') ;
-    s(I).thresh100 = find(cumsum(s(I).explained)>=100,1,'first') ; 
 end
-figure; 
-subplot(2,2,1) ; plot([s.thresh95_2],[s.thresh95],'ok');title('95')
-subplot(2,2,3) ; plot([s.thresh90_2],[s.thresh90],'ok');title('90')
-subplot(2,2,2) ; plot([s.thresh100_2],[s.thresh100],'ok');title('100')
-subplot(2,2,4) ; plot([s.thresh99_2],[s.thresh90_2],'ok');title('99 x 90')
 
-s = sortStruct(s,'thresh95');
-
-%%
-fh = figure('units','centimeters','position',[5 5 10 6]);
-hold on; 
-clrs = parula(numel(s)+2);
 for I = 1:numel(s)
-    txt = sprintf('S%d' , s(I).Segment ) ;
-    plot( vertcat( 0 , cumsum(s(I).explained) )  ,'-' ,'DisplayName',txt,'Color',clrs(I,:));
-end
-xlim([0.5 25])
-ylim([0 100])
-legend('location','EastOutside');
-xlabel('# PCs')
-ylabel('% variance explained')
-grid on; 
-%%
-s = sortStruct(s,'Segment');
-
-rmat = NaN( 30 , numel(s));
-for I = 1:numel(s)
-    rmat(:,I) = vertcat( 0 , cumsum(s(I).explained(1:29)) ) ;
+    rmat(:,I) = vertcat( 0 , cumsum(s(I).explained(1:fewest_dim)));
 end
 fh = figure('units','centimeters','position',[5 5 8 6]);
 hold on; 
 imagesc(rmat);
-colorbar;
+colorbar
 for I = 1:numel(s)
     line([I-0.5 I+0.5] , [ s(I).thresh99 s(I).thresh99] ,'Color','k')
 end
 ylabel('# of principal components')
 xlabel('Segment')
 axis tight;
+
+%%
+% % % % 
+% % % % 
+% % % % %%
+% % % % for I = 1:12
+% % % %     s(I).thresh95_2 = find(cumsum(s(I).explained2)>95,1,'first') ;
+% % % %     s(I).thresh95 = find(cumsum(s(I).explained)>95,1,'first') ; 
+% % % %     s(I).thresh90_2 = find(cumsum(s(I).explained2)>90,1,'first') ;
+% % % %     s(I).thresh90 = find(cumsum(s(I).explained)>90,1,'first') ; 
+% % % %     s(I).thresh99_2 = find(cumsum(s(I).explained2)>99,1,'first') ;
+% % % %     s(I).thresh99 = find(cumsum(s(I).explained)>99,1,'first') ; 
+% % % %     s(I).thresh100_2 = find(cumsum(s(I).explained2)>=100,1,'first') ;
+% % % %     s(I).thresh100 = find(cumsum(s(I).explained)>=100,1,'first') ; 
+% % % % end
+% % % % figure; 
+% % % % subplot(2,2,1) ; plot([s.thresh95_2],[s.thresh95],'ok');title('95')
+% % % % subplot(2,2,3) ; plot([s.thresh90_2],[s.thresh90],'ok');title('90')
+% % % % subplot(2,2,2) ; plot([s.thresh100_2],[s.thresh100],'ok');title('100')
+% % % % subplot(2,2,4) ; plot([s.thresh99_2],[s.thresh90_2],'ok');title('99 x 90')
+% % % % 
+% % % % s = sortStruct(s,'thresh95');
+% % % % 
+% % % % %%
+% % % % fh = figure('units','centimeters','position',[5 5 10 6]);
+% % % % hold on; 
+% % % % clrs = parula(numel(s)+2);
+% % % % for I = 1:numel(s)
+% % % %     txt = sprintf('S%d' , s(I).Segment ) ;
+% % % %     plot( vertcat( 0 , cumsum(s(I).explained) )  ,'-' ,'DisplayName',txt,'Color',clrs(I,:));
+% % % % end
+% % % % xlim([0.5 25])
+% % % % ylim([0 100])
+% % % % legend('location','EastOutside');
+% % % % xlabel('# PCs')
+% % % % ylabel('% variance explained')
+% % % % grid on;
--- Analysis/Lucas/EpistasisLBC__CompareUnfitFreqBetweenFit_VS_EntireLibrary__MakeFigures.m
+++ Analysis/Lucas/EpistasisLBC__CompareUnfitFreqBetweenFit_VS_EntireLibrary__MakeFigures.m
@@ -6,7 +6,7 @@
 %     save( sprintf('Segment%d.mat' , I ) ,  'q');
 % end
 
-%%
+%%  make inidividual segment figures
 figbasename = 'CompareUnfitFreqBetweenFit_VS_EntireLibrary_';
 segment_files = dir('Segment*.mat');
 for SegFileI = 1:numel(segment_files)
@@ -54,12 +54,12 @@ for SegFileI = 1:numel(segment_files)
     between_pairs = NaN(N,1);
     from_Scer  = NaN(N,1);
     for I = 1:N
-        between_pairs(I) = mean( cell2mat(R.FitnessDistributions(  R.HammingDistances == I )) < 0.2);
-        from_Scer(I) = mean(T.s( T.DistFromScer==I)  < 0.2);
+        between_pairs(I) = 100 * mean( cell2mat(R.FitnessDistributions(  R.HammingDistances == I )) < 0.2);
+        from_Scer(I) = 100 * mean(T.s( T.DistFromScer==I)  < 0.2);
     end
     
     fh = figure('units','centimeters','position',[5 5 7 5 ]);
-    bh = bar( 100* [between_pairs ,   from_Scer  ] );
+    bh = bar( [between_pairs ,   from_Scer  ] );
     bh(1).FaceColor=[0 0 1] ; bh(1).EdgeColor=[0 0 1] ;
     bh(2).FaceColor=[1 0 0] ;  bh(2).EdgeColor=[1 0 0] ;
     bh(1).BarWidth = 1 ;
@@ -79,10 +79,15 @@ for SegFileI = 1:numel(segment_files)
     %
     
     fh = figure('units','centimeters','position',[5 5 7 5 ]);
-    plot( 1:N ,  100* ((from_Scer-between_pairs) ./ from_Scer)  ,'ok','MarkerFaceColor',[.7 .7 .7])
+    Y = ((from_Scer-between_pairs) ./ from_Scer)  ;  %  as a percent
+    Y =  from_Scer - between_pairs  ;  % just the difference
+    plot( 1:N ,  Y ,'ok','MarkerFaceColor',[.7 .7 .7])
     %xlim([0.9 7.5])
     set(gca,'xtick',0:100)
     set(gca,'ytick',-100:10:100)
+    if (range(Y)<30)
+        set(gca,'ytick',-100:5:100)
+    end
     set(gca,'Ygrid','on')
     axis tight; 
     ylabel('% intersegnental epistasis')
@@ -97,3 +102,130 @@ for SegFileI = 1:numel(segment_files)
     close;    
 end
 
+%% make one figure combining data for all segments
+
+figname = 'CompareUnfitFreqBetweenFit_VS_EntireLibrary.eps';
+delete(figname);
+segment_files = dir('Segment*.mat');
+
+dist_at_which_to_calc = 7 ; 
+fast_thresh  = 0.4  ;
+slow_thresh  = 0.2 ; 
+
+DS = dataset();
+DS.SegN = NaN( 12*15 , 1);
+DS.pct_unfit_between_pairs = NaN( 12*15 , 1);
+DS.pct_unfit_from_Scer   = NaN( 12*15 , 1);
+DS.dist_at_which_to_calc = NaN(12*15,1);
+c = 0;
+for I = 1:numel(segment_files)
+    load( [ segment_files(I).folder filesep segment_files(I).name]);
+    for dist_at_which_to_calc = 1:15 ; 
+        c = c+1 ; 
+        DS.SegN(c) = q.SegN ;   
+        DS.pct_unfit_between_pairs(c) = 100*mean( cell2mat(q.R.FitnessDistributions(  q.R.HammingDistances == dist_at_which_to_calc )) < slow_thresh );
+        DS.pct_unfit_from_Scer(c) = 100*mean(q.T.s( q.T.DistFromScer==dist_at_which_to_calc)  < slow_thresh );    
+        DS.dist_at_which_to_calc(c) = dist_at_which_to_calc;
+    end
+end
+DS.difference  =  DS.pct_unfit_from_Scer - DS.pct_unfit_between_pairs ;
+%DS.difference  = 100* ((DS.pct_unfit_from_Scer-DS.pct_unfit_between_pairs) ./ DS.pct_unfit_from_Scer)  ;
+
+DS = DS( ~isnan(DS.difference) ,:);
+%% figure
+% fh = figure('units','centimeters','position',[5 5 9 5 ]);
+% bar( double(DS(:,2:end))' )
+% set(gca,'xticklabel' , {'between pairs' 'from S. cer' 'difference'})
+% ylabel('difference')
+% set(gca,'Ygrid','on')
+% title('% unfit at dist=7')
+values_to_plot = {'difference' 'pct_unfit_from_Scer' 'pct_unfit_between_pairs'} ;
+
+
+G = grpstats( DS , 'dist_at_which_to_calc' ,{'mean' 'median' 'std'} )
+DS = DS( DS.dist_at_which_to_calc < 12 ,:);
+nu = numel(unique(DS.dist_at_which_to_calc));
+
+d=0.15 ; 
+
+fh = figure('units','centimeters','position',[5 5 7 7 ]); 
+hold on ;
+
+
+bh = boxplot(  DS.difference , DS.dist_at_which_to_calc ,'Positions', (1:nu)  ,'PlotStyle','compact','Symbol','','Color',[.75 .75 .75],'LabelOrientation','horizontal' )
+
+
+bh = boxplot(  DS.pct_unfit_from_Scer , DS.dist_at_which_to_calc ,'Positions', (1:nu)-d  ,'PlotStyle','compact','Symbol','','Color',[1 0.75 0.75 ],'LabelOrientation','horizontal' )
+
+bh = boxplot(  DS.pct_unfit_between_pairs , DS.dist_at_which_to_calc ,'Positions', (1:nu)+d  ,'PlotStyle','compact','Symbol','','Color',[0.75 0.75 1 ],'LabelOrientation','horizontal' )
+
+plot( G.dist_at_which_to_calc , G.median_difference ,'.k','MarkerSize',15)
+[fitresult1, gof1] = fit( G.dist_at_which_to_calc, G.median_difference , fittype( 'power1' ) );
+plot( 0:.1:100  , fitresult1(  0:.1:100 ) ,'-k')
+
+plot( (1:nu)-d  , G.median_pct_unfit_from_Scer ,'.','Color',[1 0 0 ],'MarkerSize',15)
+[fitresult2, gof2] = fit( G.dist_at_which_to_calc, G.median_pct_unfit_from_Scer , fittype( 'power1' ) );
+plot( 0:.1:100  , fitresult2(  0:.1:100 ) ,'-','Color',[1 0 0 ])
+
+
+plot( (1:nu)+d  , G.median_pct_unfit_between_pairs ,'.','Color',[0 0 1 ],'MarkerSize',15)
+[fitresult3, gof3] = fit( G.dist_at_which_to_calc, G.median_pct_unfit_between_pairs , fittype( 'power1' ) );
+plot( 0:.1:100  , fitresult3(  0:.1:100 ) ,'-','Color',[0 0 1 ])
+
+
+set(gca,'Ygrid','on')
+ylabel('Amount of epsitasis')
+set(gca,'ytick',[0   10:10:100 ] );
+set(gca,'yticklabel',  arrayfun(@(X)sprintf('%d%%',X) , get(gca,'ytick') ,'UniformOutput',false) );
+ylim([-1 40])
+xlim([1.3 11.6])
+%set(gca,'xtick',1:2:20) ; set(gca,'xticklabel',1:2:20)
+xlabel('Mutational distance')
+%print('-dpng','Inter-segmental epistasis increases faster than intrasegmental 1.png','-r600');
+%close;
+
+fprintf('Diff b=%0.03f R^2=%0.02f\n' , fitresult1.b , gof1.rsquare);
+fprintf('From Scer b=%0.03f R^2=%0.02f\n' , fitresult2.b , gof2.rsquare); 
+fprintf('From pairs b=%0.03f R^2=%0.02f\n' , fitresult3.b , gof3.rsquare); 
+
+%% Fit all segment seperatly
+usegs = unique(DS.SegN);
+R = dataset();
+R.SegN = NaN(12,1);
+for I = 1:numel(usegs)
+    idx = ( DS.SegN == usegs(I)  ) ;
+    R.SegN(I) = usegs(I) ;
+    [R.fitresultPAIR{I}, R.gofPAIR{I}] = fit( DS.dist_at_which_to_calc(idx), DS.pct_unfit_between_pairs(idx) , fittype( 'power1' ) );
+    [R.fitresultSCER{I}, R.gofSCER{I}] = fit( DS.dist_at_which_to_calc(idx), DS.pct_unfit_from_Scer(idx) , fittype( 'power1' ) );
+    [R.fitresultDIFF{I}, R.gofDIFF{I}] = fit( DS.dist_at_which_to_calc(idx), DS.difference(idx) , fittype( 'power1' ) );
+    R.diff_b(I) = R.fitresultDIFF{I}.b ;
+    R.pair_b(I) = R.fitresultPAIR{I}.b ;
+    R.scer_b(I) = R.fitresultSCER{I}.b ;
+    R.diff_r2(I) = R.gofDIFF{I}.rsquare ;
+    R.pair_r2(I) = R.gofPAIR{I}.rsquare ;
+    R.scer_r2(I) = R.gofSCER{I}.rsquare ;    
+end
+
+% only decent fits
+R = R( R.scer_r2>0.5,:) ; 
+
+figure; hold on; 
+subplot(1,2,1);
+%plot( R.diff_b , R.scer_b ,'ok','MarkerFaceColor',[.7 .7 .7]);
+text( R.diff_b , R.scer_b , arrayfun(@num2str, R.SegN) )
+xlabel('difference')
+ylabel('S cer ')
+xlim([0 6])
+ylim(xlim)
+line(xlim,xlim)
+
+subplot(1,2,2);
+%plot( R.diff_b , R.pair_b ,'ok','MarkerFaceColor',[.7 .7 .7]);
+text( R.diff_b , R.pair_b , arrayfun(@num2str, R.SegN) )
+xlabel('difference')
+ylabel('Pairs')
+
+suptitle('b exponent from fit')
+xlim([0 6])
+ylim(xlim)
+line(xlim,xlim)
