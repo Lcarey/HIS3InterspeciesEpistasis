1427d124f232834dcc3fdfa40955f5bf62243f75
Karen Sarkisyan <karen.s.sarkisyan@gmail.com>
2017-05-17 18:48:21 +0200

    minor changes

---
+++ Analysis/Karen/files_dump/pymol_and_pdb_functions.py
@@ -0,0 +1,35 @@
+def calc_dist_matrix(chain1, chain2, stripping=True, mode='min'):
+    """Returns a matrix of C-alpha distances between two chains"""
+    chain1_residues = rstrip_non_aa_residues(list(chain1.get_residues()))
+    chain2_residues = rstrip_non_aa_residues(list(chain2.get_residues()))
+    answer = np.zeros((len(chain1_residues), len(chain2_residues)), np.float)
+    for row, residue_one in enumerate(chain1_residues):
+        for col, residue_two in enumerate(chain2_residues) :
+            answer[row, col] = get_distance_between_residues(residue_one, residue_two, mode=mode)
+    return answer
+
+
+def get_minimal_distances_in_a_complex(chains):
+    distances = []
+    chains = [chain for chain in chains if chain.id in ascii_letters]
+    for chain1 in chains:
+        for chain2 in chains:
+            distances.append(calc_dist_matrix(chain1, chain2))
+    return np.amin(np.array(distances), axis=0)
+
+def strip_non_aa_residues(residues):
+    for index, residue in enumerate(residues):
+        if residue.resname not in aa3:
+            pass
+        else:
+            break
+    residues = residues[index:]
+    return residues
+
+
+def rstrip_non_aa_residues(residues):
+    for index, residue in enumerate(residues):
+        if residue.resname not in aa3:
+            break
+    residues = residues[:index]
+    return residues
