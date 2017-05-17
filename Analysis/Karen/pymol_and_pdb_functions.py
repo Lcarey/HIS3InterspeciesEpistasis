5d86a2e1257a6a1b64b4ca1b6bc50dde596b5b6e
Karen Sarkisyan <karen.s.sarkisyan@gmail.com>
2017-05-17 22:09:55 +0200

    minor changes

--- Analysis/Karen/pymol_and_pdb_functions.py
+++ Analysis/Karen/pymol_and_pdb_functions.py
@@ -1,3 +1,30 @@
+import numpy as np
+
+def get_distance_between_atoms(atom1, atom2):
+    diff_vector  = atom1.coord - atom2.coord
+    return np.sqrt(np.sum(diff_vector * diff_vector))
+
+
+def get_distance_between_residues(residue_one, residue_two, mode='min'):
+    assert mode in ['CA', 'min']
+    if mode == 'CA':
+        diff_vector  = residue_one["CA"].coord - residue_two["CA"].coord
+        return np.sqrt(np.sum(diff_vector * diff_vector))
+    if mode == 'min':
+        distances = []
+        for atom1 in residue_one.get_atom():
+            for atom2 in residue_two.get_atom():
+                distances.append(get_distance_between_atoms(atom1, atom2))
+        return min(distances)
+
+    
+def get_distance_to_other_residues(residue, other_residues, mode='min'):
+    distances = np.zeros(len(other_residues))
+    for index, other_residue in enumerate(other_residues):
+        distances[index] = get_distance_between_residues(residue, other_residue, mode=mode)
+    return distances
+
+
 def calc_dist_matrix(chain1, chain2, stripping=True, mode='min'):
     """Returns a matrix of C-alpha distances between two chains"""
     chain1_residues = rstrip_non_aa_residues(list(chain1.get_residues()))
